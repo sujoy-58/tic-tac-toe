8a0c037dcb733cd1beca5ee8d09b740ce9feb098
sujoy <sujoymaji16@gmail.com>
2024-09-08 12:37:56 +0530

    UI changes and enter button implement

--- script.js
+++ script.js
@@ -49,30 +49,57 @@
         message.classList.remove("hide");
     };
     
-    const cheakWinner = () => {
-        for (let pattern of winningCombinations) {
-            const [pos1, pos2, pos3] = pattern.map(index => boxes[index].innerText);
-    
-            if (pos1 && pos1 === pos2 && pos2 === pos3) {
-                disableBoxes();
-                handleGameResult(pos1);
-                return; // Exit the function, as the game has been won
-            }
-        }
-    
-        // Check for a draw
-        if ([...boxes].every(box => box.innerText !== '')) {
-            handleGameResult('draw');
-        }
-    };
-    resetBtn.addEventListener("click", () => {
-        activePlayer = true;
-        enableBoxes();
-        handleGameResult('');
-        message.classList.add("hide");
-        para.innerHTML = '';
-    
-    });
-    
 // });
+
+const cheakWinner = () => {
+    for (let pattern of winningCombinations) {
+        const [a, b, c] = pattern;
+
+        const box1 = boxes[a].innerText;
+        const box2 = boxes[b].innerText;
+        const box3 = boxes[c].innerText;
+
+        if (box1 && box1 === box2 && box2 === box3) {
+            // Add highlight class to winning boxes
+            boxes[a].classList.add("highlight");
+            boxes[b].classList.add("highlight");
+            boxes[c].classList.add("highlight");
+
+            disableBoxes();
+            handleGameResult(box1);
+            return; // Exit the function, as the game has been won
+        }
+    }
+
+    // Check for a draw
+    if ([...boxes].every(box => box.innerText !== '')) {
+        handleGameResult('draw');
+    }
+};
+
+// Reset function needs to remove the highlight class
+// Reset button functionality
+resetBtn.addEventListener("click", () => {
+    resetGame();
+});
+
+// Trigger reset when Enter key is pressed
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        resetGame();
+    }
+});
+
+const resetGame = () => {
+    activePlayer = true;
+    enableBoxes();
+    handleGameResult('');
+    message.classList.add("hide");
+    para.innerHTML = '';
+
+    // Remove highlight class from all boxes
+    boxes.forEach(box => box.classList.remove("highlight"));
+};
+
+
 
